f0d0de72989823d5758a0b7310ce79ce54b01039
aisaman581 <89404575+aisaman581@users.noreply.github.com>
2022-11-17 14:59:48 -0500

    Refactored compute_metrics

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,64 +1,79 @@
-import sys
-from urllib import request
+requests_sent = 0
+requests_received = 0
+replies_sent = 0
+replies_received = 0
+requests_sent_bytes = 0
+requests_received_bytes = 0
+requests_sent_data = 0
+requests_received_data = 0
+rtt_seq_num_list = []
+roundtrip_time = 0
+rd_seq_num_list = []
+reply_delay = 0
+total_hops = 0
+
+def rtt_compute(rtt_seq_num_list, i):
+    global roundtrip_time
+    global total_hops
+    for seq in rtt_seq_num_list:
+                if (i[6] == seq[1]):
+                    roundtrip_time += (i[0] - seq[0])
+                    total_hops += ((seq[2] - i[7])+ 1)
+                    rtt_seq_num_list.remove(seq)
+
+def rd_compute(rd_seq_num_list, i):
+    global reply_delay
+    for seq in rd_seq_num_list:
+                if (i[6] == seq[1]):
+                    reply_delay += (i[0] - seq[0])
+                    rd_seq_num_list.remove(seq)   
 
 def compute(L):
-    requests_sent = 0
-    requests_received = 0
-    replies_sent = 0
-    replies_received = 0
-    requests_sent_bytes = 0
-    requests_received_bytes = 0
-    requests_sent_data = 0
-    requests_received_data = 0
-    rtt_seq_num_list = []
-    roundtrip_time = 0
-    rd_seq_num_list = []
-    reply_delay = 0
-    total_hops = 0
+    global requests_sent
+    global requests_sent_bytes
+    global requests_sent_data
+    global requests_received
+    global requests_received_bytes
+    global requests_received_data
+    global replies_sent
+    global replies_received
+
     for i in L:
         if (i[1] == host_ip and i[5] == "Request"):
             requests_sent += 1
             requests_sent_bytes += i[4]
             requests_sent_data += (i[4] - 42)
             rtt_seq_num_list.append([i[0], i[6], i[7]])
-        if (i[1] != host_ip and i[5] == "Request"):
+        elif (i[1] != host_ip and i[5] == "Request"):
             requests_received += 1
             requests_received_bytes += i[4]
             requests_received_data += (i[4] - 42)
             rd_seq_num_list.append([i[0], i[6]])
-        if (i[1] == host_ip and i[5] == "Reply"):
+        elif (i[1] == host_ip and i[5] == "Reply"):
             replies_sent += 1
-            for seq in rd_seq_num_list:
-                if (i[6] == seq[1]):
-                    reply_delay += (i[0] - seq[0])
-                    rd_seq_num_list.remove(seq)   
-        if (i[1] != host_ip and i[5] == "Reply"):
+            rd_compute(rd_seq_num_list, i)
+        else:
             replies_received += 1
-            for seq in rtt_seq_num_list:
-                if (i[6] == seq[1]):
-                    roundtrip_time += (i[0] - seq[0])
-                    total_hops += ((seq[2] - i[7])+ 1)
-                    rtt_seq_num_list.remove(seq)
+            rtt_compute(rtt_seq_num_list, i)
 
     avg_rtt = (roundtrip_time/requests_sent)
     throughput = ((requests_sent_bytes/1000)/roundtrip_time)
     goodput = ((requests_sent_data/1000)/roundtrip_time)
     delay = (reply_delay/requests_received)
 
-    print("Echo Requests Sent: " + str(requests_sent))
-    print("Echo Requests Received: " + str(requests_received))  
-    print("Echo Replies Sent: " + str(replies_sent))  
-    print("Echo Replies Received: " + str(replies_received))           
-    print("Echo Request Bytes Sent (bytes): " + str(requests_sent_bytes))
-    print("Echo Request Data Sent (bytes): " + str(requests_sent_data))
-    print("Echo Request Bytes Received (bytes): " + str(requests_received_bytes))
-    print("Echo Request Data Received (bytes): " + str(requests_received_data))
+    print("Echo Requests Sent,Echo Requests Received,Echo Replies Sent,Echo Replies Received")
+    print(str(requests_sent) + "," + str(requests_received) + "," + str(replies_sent) + "," + str(replies_received))   
+          
+    print("Echo Request Bytes Sent (bytes),Echo Request Data Sent (bytes)")
+    print(str(requests_sent_bytes) + "," + str(requests_sent_data))
+    print("Echo Request Bytes Received (bytes),Echo Request Data Received (bytes)")
+    print(str(requests_received_bytes) + "," + str(requests_received_data))
 
-    print("Average RTT (milliseconds): " + str(avg_rtt * 1000))
-    print("Echo Request Throughput (kB/sec): " + str(throughput))
-    print("Echo Request Goodput (kB/sec): " + str(goodput))
-    print("Average Reply Delay (microseconds): "+ str(delay * 1000000))
-    print("Average Echo Request Hop Count: " + str(total_hops/requests_sent))
+    print("Average RTT (milliseconds)," + str(avg_rtt * 1000))
+    print("Echo Request Throughput (kB/sec)," + str(throughput))
+    print("Echo Request Goodput (kB/sec)," + str(goodput))
+    print("Average Reply Delay (microseconds),"+ str(delay * 1000000))
+    print("Average Echo Request Hop Count," + str(total_hops/requests_sent))
 
 L=[
 [0.000000, "192.168.200.1", "192.168.100.1", "ICMP", 74, "Request", "14/3584", 128],
