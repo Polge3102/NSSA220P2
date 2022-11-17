7a5a607f45b25a77b66c5050cea4883da84e813e
michael-tomko <89418516+michael-tomko@users.noreply.github.com>
2022-11-17 14:27:53 -0500

    Update filter_packets.py
    Filters out ICMP echo packets, could add the hex part later.

--- filter_packets.py
+++ filter_packets.py
@@ -1,3 +1,14 @@
-def filter() :
+def filter(nodeFilename, outputFilename) :
 	print('called filter function in filter_packets.py')
-	
+	captures = open(nodeFilename, 'r')
+	filtered = open(outputFilename, 'w')
+	line = captures.readline()
+	while line:
+		line = captures.readline()
+		if line.count('Echo') >= 1:
+			filtered.write(line)
+			line = captures.readline()
+				
+		
+	captures.close()
+	filtered.close()
