b1366aa3375128bfd883efe266c591f858f633ef
aisaman581 <89404575+aisaman581@users.noreply.github.com>
2022-11-29 14:55:27 -0500

    Made som adjustsments
    Added a comment to specify how how the compute function expects the data to be formatted

--- compute_metrics.py
+++ compute_metrics.py
@@ -16,15 +16,15 @@ def rtt_compute(rtt_seq_num_list, i):
     global roundtrip_time
     global total_hops
     for seq in rtt_seq_num_list:
-                if (i[6] == seq[1]):
+                if (i[5] == seq[1]):
                     roundtrip_time += (i[0] - seq[0])
-                    total_hops += ((seq[2] - i[7])+ 1)
+                    total_hops += ((seq[2] - i[6])+ 1)
                     rtt_seq_num_list.remove(seq)
 
 def rd_compute(rd_seq_num_list, i):
     global reply_delay
     for seq in rd_seq_num_list:
-                if (i[6] == seq[1]):
+                if (i[5] == seq[1]):
                     reply_delay += (i[0] - seq[0])
                     rd_seq_num_list.remove(seq)   
 
@@ -39,17 +39,17 @@ def compute(packet_list):
     global replies_received
 
     for i in packet_list:
-        if (i[1] == host_ip and i[5] == "Request"):
+        if (i[1] == host_ip and i[4] == "Request"):
             requests_sent += 1
-            requests_sent_bytes += i[4]
-            requests_sent_data += (i[4] - 42)
-            rtt_seq_num_list.append([i[0], i[6], i[7]])
-        elif (i[1] != host_ip and i[5] == "Request"):
+            requests_sent_bytes += i[3]
+            requests_sent_data += (i[3] - 42)
+            rtt_seq_num_list.append([i[0], i[5], i[6]])
+        elif (i[1] != host_ip and i[4] == "Request"):
             requests_received += 1
-            requests_received_bytes += i[4]
-            requests_received_data += (i[4] - 42)
-            rd_seq_num_list.append([i[0], i[6]])
-        elif (i[1] == host_ip and i[5] == "Reply"):
+            requests_received_bytes += i[3]
+            requests_received_data += (i[3] - 42)
+            rd_seq_num_list.append([i[0], i[5]])
+        elif (i[1] == host_ip and i[4] == "Reply"):
             replies_sent += 1
             rd_compute(rd_seq_num_list, i)
         else:
@@ -75,19 +75,21 @@ def compute(packet_list):
     print("Average Reply Delay (microseconds),"+ str(delay * 1000000))
     print("Average Echo Request Hop Count," + str(total_hops/requests_sent))
 
+# Expecting a list of lists. Each list contains 7 values in a specific order. The time field to six decimal places, the source IP as a string, the destination IP as a string,
+# the 
 L=[
-[0.000000, "192.168.200.1", "192.168.100.1", "ICMP", 74, "Request", "14/3584", 128],
-[0.003678, "192.168.100.1", "192.168.200.1", "ICMP", 74, "Reply", "14/3584", 126],
-[1.204302, "192.168.100.1", "192.168.200.1", "ICMP", 74, "Request", "19/3584", 126],
-[1.204322, "192.168.200.1", "192.168.100.1", "ICMP", 74, "Reply", "19/3584", 128],
-[1.500230, "192.168.200.1", "192.168.100.1", "ICMP", 74, "Request", "23/3584", 128],
-[1.510221, "192.168.100.1", "192.168.200.1", "ICMP", 74, "Reply", "23/3584", 127],
-[1.952140, "192.168.100.1", "192.168.200.1", "ICMP", 74, "Request", "28/3584", 126],
-[1.952145, "192.168.200.1", "192.168.100.1", "ICMP", 74, "Reply", "28/3584", 128],
-[2.001922, "192.168.200.1", "192.168.100.1", "ICMP", 74, "Request", "34/3584", 128],
-[2.005032, "192.168.100.1", "192.168.200.1", "ICMP", 74, "Reply", "34/3584", 128],
-[2.230000, "192.168.100.1", "192.168.200.1", "ICMP", 74, "Request", "40/3584", 126],
-[2.230005, "192.168.200.1", "192.168.100.1", "ICMP", 74, "Reply", "40/3584", 128]
+[0.000000, "192.168.200.1", "192.168.100.1", 74, "Request", "14", 128],
+[0.003678, "192.168.100.1", "192.168.200.1", 74, "Reply", "14", 126],
+[1.204302, "192.168.100.1", "192.168.200.1", 74, "Request", "19", 126],
+[1.204322, "192.168.200.1", "192.168.100.1", 74, "Reply", "19", 128],
+[1.500230, "192.168.200.1", "192.168.100.1", 74, "Request", "23", 128],
+[1.510221, "192.168.100.1", "192.168.200.1", 74, "Reply", "23", 127],
+[1.952140, "192.168.100.1", "192.168.200.1", 74, "Request", "28", 126],
+[1.952145, "192.168.200.1", "192.168.100.1", 74, "Reply", "28", 128],
+[2.001922, "192.168.200.1", "192.168.100.1", 74, "Request", "34", 128],
+[2.005032, "192.168.100.1", "192.168.200.1", 74, "Reply", "34", 128],
+[2.230000, "192.168.100.1", "192.168.200.1", 74, "Request", "40", 126],
+[2.230005, "192.168.200.1", "192.168.100.1", 74, "Reply", "40", 128]
 ]
 host_ip = "192.168.200.1"
 compute(L)
